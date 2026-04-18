8f384f07f8ca06a711389bfa47b1a2914aec6abf
LucaOrtellado <lucaorte2004@gmail.com>
2026-04-18 16:40:40 -0300

    Ejercicio 1

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,10 @@
+const nombre = 'Luca';
+const edad = 21;
+const materia = 'Desarrollo de software';
+
+console.log(`Hola soy ${nombre}, tengo ${edad} años y estoy cursando ${materia}`);
+let cont = 0;
+for (let i=0; i<3; i++){
+    cont++;
+}
+console.log(`El resultado del contador es: ${cont}`);
