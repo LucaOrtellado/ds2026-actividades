c50bc6265dabd2af19d50b4c511e0a07d1f2e9dc
LucaOrtellado <lucaorte2004@gmail.com>
2026-08-16 17:20:20 -0300

    C14-Backend

---
+++ ejercicios/c14-backend/frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer className="bg-body-tertiary text-center py-3 mt-4">
+      <p className="mb-0">Librería</p>
+    </footer>
+  )
+}
+export default Footer
